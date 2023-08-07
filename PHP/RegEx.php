433a973b9273a392e9bc7a765e3ad692be2588cd
SORBO Michele 74428 @74428 <michele.sorbo@consulenti.csi.it>
2023-08-07 10:23:44 +0200

    regEx

--- PHP/RegEx.php
+++ PHP/RegEx.php
@@ -1,9 +1,14 @@
 <?php 
 
+//Documentazione: 
+//HTM.it: https://www.html.it/pag/60273/le-espressioni-regolari-in-php/
+//W3S: https://www.w3schools.com/php/php_regex.asp
+//Tutorial Republic: https://www.tutorialrepublic.com/php-tutorial/php-regular-expressions.php
+//PHP.net: https://www.php.net/manual/en/function.preg-match.php
+
 $testo = "Oggi è una splendida giornata, possiamo andare al mare";
 
-$nome = "Michele";
-$cap = "00600";
+
 
 /*
 CARATTERI REG EX
@@ -20,15 +25,20 @@ i fuori dal / attiva la modalità case insensitive
 {} inizio e fine dell'intervallo. {minimo, massimo} {masismo}
 */
 
+$nome = "Michele";
+$cap = "00600";
+$cf = "RSSMRR23L56B657S";
+
 $pattern = "/^mi/i"; //Pattern per testare l'espressione regolare
 $pattern_cap = "/^[0-9]{5}$/"; //Regola per testare la corretta scrittura di un CAP
-$pattern_CF = ""; //CCC CCC 11 C 11 C 111 C --> SRB MHL 79 L 26 B 715 S 
+$pattern_CF = "/^[A-Z]{6}[0-9]{2}[A-Z]{1}[0-9]{2}[A-Z]{1}[0-9]{3}[A-Z]{1}$/"; //CCC CCC 11 C 11 C 111 C  SRB MHL 79 L 26 B 715 S 
+$pattern_mail = "/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,4})+$/";
 
 //Vado a testare il Pattern sulla stringa, la funzione ritorna un booleano
 // 0 (false) pattern non trovato
 // 1 (true) pattern trovato
 
-if(preg_match($pattern_cap, $cap)){
+if(preg_match($pattern_CF, $cf)){
     echo "Pattern trovato";
 }else{
     echo "Pattern non trovato";
