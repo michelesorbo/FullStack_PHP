2baef78437c2859911be9bc54ace1f756b2ef789
SORBO Michele 74428 @74428 <michele.sorbo@consulenti.csi.it>
2023-08-07 16:50:55 +0200

    esercizio_2

---
+++ PHP/es_file/dashboard.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Benvenuto nella tua pagina persoanle</h1>
+</body>
+</html>
---
+++ PHP/es_file/lgoin_controllo.php
@@ -0,0 +1,8 @@
+<?php 
+
+include('funzioni.php');
+
+echo md5($_POST["pwd"]);
+
+
+?>
